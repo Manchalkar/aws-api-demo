974b94ec8370fe0a7276f85f98e4aed62519aa6b
Shubham Manchalkar <smanchalkar@codal.com>
2021-09-20 10:58:53 +0530

    updated git ignore

--- Etools/Startup.cs
+++ Etools/Startup.cs
@@ -112,7 +112,7 @@ namespace Etools
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
